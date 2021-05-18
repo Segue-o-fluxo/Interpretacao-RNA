a0014c7a282fad20248943cae1569c7b6e13ff0b
William Amorim <william.rappel.ds@gmail.com>
2021-05-17 23:56:46 -0300

    Final

--- William.R
+++ William.R
@@ -3,6 +3,7 @@ library(dplyr)
 library(keras)
 library(ggplot2)
 library(latex2exp)
+library(DT)
 
 
 ########## Importando os Dados ##########
@@ -25,6 +26,10 @@ mod_bom  <- load_model_tf("Rede Boa")
 source("Metricas.R")
 
 
+########## Dataframe para comparar resultados ##########
+resultados <- NULL
+
+
 ########## Procedimento 1: Assume erros i.i.d. ~ N(0, sigma^2) ##########
 # Predicoes pontuais
 yhat_treino_ruim <- mod_ruim %>% predict_on_batch(as.matrix(x_treino)) %>% as.vector()
@@ -38,25 +43,16 @@ res_treino_bom   <- y_treino - yhat_treino_bom
 sigma2hat_ruim <- var(res_treino_ruim)
 sigma2hat_bom  <- var(res_treino_bom)
 # Limites inferiores
-linf_treino_ruim <- yhat_treino_ruim - qnorm(0.975)*sqrt(sigma2hat_ruim)
-linf_treino_bom  <- yhat_treino_bom  - qnorm(0.975)*sqrt(sigma2hat_bom)
 linf_teste_ruim  <- yhat_teste_ruim  - qnorm(0.975)*sqrt(sigma2hat_ruim)
 linf_teste_bom   <- yhat_teste_bom   - qnorm(0.975)*sqrt(sigma2hat_bom)
 # Limites superiores
-lsup_treino_ruim <- yhat_treino_ruim + qnorm(0.975)*sqrt(sigma2hat_ruim)
-lsup_treino_bom  <- yhat_treino_bom  + qnorm(0.975)*sqrt(sigma2hat_bom)
 lsup_teste_ruim  <- yhat_teste_ruim  + qnorm(0.975)*sqrt(sigma2hat_ruim)
 lsup_teste_bom   <- yhat_teste_bom   + qnorm(0.975)*sqrt(sigma2hat_bom)
 # Cobertura media
-(cob_treino_ruim <- cob_media(ind_cob(y_treino, linf_treino_ruim, lsup_treino_ruim)))
-(cob_treino_bom  <- cob_media(ind_cob(y_treino, linf_treino_bom,  lsup_treino_bom)))
 (cob_teste_ruim  <- cob_media(ind_cob(y_teste,  linf_teste_ruim,  lsup_teste_ruim)))
 (cob_teste_bom   <- cob_media(ind_cob(y_teste,  linf_teste_bom,   lsup_teste_bom)))
 # CWC
-R_treino         <- max(y_treino) - min(y_treino)
 R_teste          <- max(y_teste)  - min(y_teste)
-(cwc_treino_ruim <- cwc(amp_media_norm(linf_treino_ruim, lsup_treino_ruim, R_treino), cob_treino_ruim))
-(cwc_treino_bom  <- cwc(amp_media_norm(linf_treino_bom,  lsup_treino_bom,  R_treino), cob_treino_bom))
 (cwc_teste_ruim  <- cwc(amp_media_norm(linf_teste_ruim,  lsup_teste_ruim,  R_teste),  cob_teste_ruim))
 (cwc_teste_bom   <- cwc(amp_media_norm(linf_teste_bom,   lsup_teste_bom,   R_teste),  cob_teste_bom))
 # Funcao grafico da cobertura
@@ -74,33 +70,21 @@ dados_grid$ind_ruim <- as.factor(as.numeric(ind_cob(y_teste, linf_teste_ruim, ls
 dados_grid$ind_bom  <- as.factor(as.numeric(ind_cob(y_teste, linf_teste_bom,  lsup_teste_bom)))
 grafico_cob(ruim = T)
 grafico_cob(ruim = F)
+# Adicionando resultados
+resultados <- rbind(resultados, data.frame(Metodo    = rep("Normal", 2), 
+                                           Rede      = c("Ruim", "Boa"),
+                                           Cobertura = c(cob_teste_ruim, cob_teste_bom),
+                                           CWC       = c(cwc_teste_ruim, cwc_teste_bom)))
 
 
 ########## Procedimento 2: Realiza bootstrap nao parametrico dos residuos ##########
 # Bootstrap dos residuos
 set.seed(1)
-boot_res_treino_ruim <- matrix(sample(res_treino_ruim, 1000*nrow(treino), T), nrow(treino))
-boot_res_treino_bom  <- matrix(sample(res_treino_bom,  1000*nrow(treino), T), nrow(treino))
 boot_res_teste_ruim  <- matrix(sample(res_treino_ruim, 1000*nrow(teste), T),  nrow(teste))
 boot_res_teste_bom   <- matrix(sample(res_treino_bom,  1000*nrow(teste), T),  nrow(teste))
 # Bootstrap dos valores previstos
-boot_prev_treino_ruim <- boot_res_treino_ruim + yhat_treino_ruim
-boot_prev_treino_bom  <- boot_res_treino_bom  + yhat_treino_bom
 boot_prev_teste_ruim  <- boot_res_teste_ruim  + yhat_teste_ruim
 boot_prev_teste_bom   <- boot_res_teste_bom   + yhat_teste_bom
-# Limites de treino
-linf_treino_ruim <- numeric(nrow(treino))
-linf_treino_bom  <- numeric(nrow(treino))
-lsup_treino_ruim <- numeric(nrow(treino))
-lsup_treino_bom  <- numeric(nrow(treino))
-for(i in 1:nrow(treino)) {
-  quantis_treino_ruim   <- quantile(boot_prev_treino_ruim[i,], c(0.025, 0.975))
-  quantis_treino_bom    <- quantile(boot_prev_treino_bom[i,],  c(0.025, 0.975))
-  linf_treino_ruim[i]   <- quantis_treino_ruim[1]
-  linf_treino_bom[i]    <- quantis_treino_bom[1]
-  lsup_treino_ruim[i]   <- quantis_treino_ruim[2]
-  lsup_treino_bom[i]    <- quantis_treino_bom[2]
-}
 # Limites de teste
 linf_teste_ruim <- numeric(nrow(teste))
 linf_teste_bom  <- numeric(nrow(teste))
@@ -115,15 +99,10 @@ for(i in 1:nrow(teste)) {
   lsup_teste_bom[i]    <- quantis_teste_bom[2]
 }
 # Cobertura media
-(cob_treino_ruim <- cob_media(ind_cob(y_treino, linf_treino_ruim, lsup_treino_ruim)))
-(cob_treino_bom  <- cob_media(ind_cob(y_treino, linf_treino_bom,  lsup_treino_bom)))
 (cob_teste_ruim  <- cob_media(ind_cob(y_teste,  linf_teste_ruim,  lsup_teste_ruim)))
 (cob_teste_bom   <- cob_media(ind_cob(y_teste,  linf_teste_bom,   lsup_teste_bom)))
 # CWC
-R_treino         <- max(y_treino) - min(y_treino)
 R_teste          <- max(y_teste)  - min(y_teste)
-(cwc_treino_ruim <- cwc(amp_media_norm(linf_treino_ruim, lsup_treino_ruim, R_treino), cob_treino_ruim))
-(cwc_treino_bom  <- cwc(amp_media_norm(linf_treino_bom,  lsup_treino_bom,  R_treino), cob_treino_bom))
 (cwc_teste_ruim  <- cwc(amp_media_norm(linf_teste_ruim,  lsup_teste_ruim,  R_teste),  cob_teste_ruim))
 (cwc_teste_bom   <- cwc(amp_media_norm(linf_teste_bom,   lsup_teste_bom,   R_teste),  cob_teste_bom))
 # Grafico da cobertura
@@ -132,3 +111,12 @@ dados_grid$ind_ruim <- as.factor(as.numeric(ind_cob(y_teste, linf_teste_ruim, ls
 dados_grid$ind_bom  <- as.factor(as.numeric(ind_cob(y_teste, linf_teste_bom,  lsup_teste_bom)))
 grafico_cob(ruim = T)
 grafico_cob(ruim = F)
+# Adicionando resultados
+resultados <- rbind(resultados, data.frame(Metodo    = rep("Bootstrap NP", 2),
+                                           Rede      = c("Ruim", "Boa"),
+                                           Cobertura = c(cob_teste_ruim, cob_teste_bom),
+                                           CWC       = c(cwc_teste_ruim, cwc_teste_bom)))
+# Apresentação dos Resultados
+resultados %>%
+  mutate(across(where(is.numeric), round, digits = 3)) %>%
+  datatable()
